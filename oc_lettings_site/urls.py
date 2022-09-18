ef950e93149ff9f852d52df1b69f90dda0a42077
ferreira <guillaumeferreira00@gmail.com>
2022-09-18 12:38:19 +0200

    modification url et deplacement des views
    Renommer lettings_index.html en index.html et la vue lettings_index en index.
    Déplacer les URL de lettings et de profiles de oc_lettings_site vers les nouvelles applications, en gardant ROOT_URLCONF identique. Déplacer les vues et les modèles des lettings dans l'application "lettings".
    Créer des espaces de nom pour les URL.
    Reproduire ces étapes pour profiles.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from .views import index_old
+from profiles.views import profile, profiles_index
+from lettings.views import index, letting
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("lettings/", views.lettings_index, name="lettings_index"),
-    path("lettings/<int:letting_id>/", views.letting, name="letting"),
-    path("profiles/", views.profiles_index, name="profiles_index"),
-    path("profiles/<str:username>/", views.profile, name="profile"),
+    path("", index_old, name="index_old"),
+    path("lettings/", index, name="index"),
+    path("lettings/<int:letting_id>/", letting, name="letting"),
+    path("profiles/", profiles_index, name="profiles_index"),
+    path("profiles/<str:username>/", profile, name="profile"),
     path("admin/", admin.site.urls),
 ]
