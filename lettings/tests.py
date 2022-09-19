495035528aeec53d7bb53d17224c3665e9479ce4
ferreira <guillaumeferreira00@gmail.com>
2022-09-19 06:36:35 +0200

    Update tests.py

--- lettings/tests.py
+++ lettings/tests.py
@@ -6,16 +6,17 @@ from .models import Address, Letting
 
 @pytest.mark.django_db
 def test_index():
-    url = reverse('index')
+    url = reverse("index")
     client = Client()
     response = client.get(url)
     assert response.status_code == 200
 
     assert b"Lettings" in response.content
 
+
 @pytest.mark.django_db
 def test_letting():
     client = Client()
-    url = reverse('letting', kwargs={'letting_id': 1})
+    url = reverse("letting", kwargs={"letting_id": 1})
     response = client.get(url)
     assert response.status_code == 200
