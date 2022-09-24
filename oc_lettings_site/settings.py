292063d826a17a774aee2c746b05f03d7e3e5473
ferreira <guillaumeferreira00@gmail.com>
2022-09-24 14:45:20 +0200

    Update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "oc-lettings-60.herokuapp.com"]
 
